5e05083dd7aea59f5d91297aa11d22b9e118bdc9
Isaac Vallee <isaacvallee@gmail.com>
2017-09-27 00:03:21 +0000

    init knex

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+const settings = require('./settings');
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: settings.hostname,
+      user: settings.user,
+      password: settings.password,
+      database: settings.database,
+      port: settings.port
+    }
+  }
+};
